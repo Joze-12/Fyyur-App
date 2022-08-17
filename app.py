7d18c5a6903b08983a9897c3b338457ed22535bc
Joze <josephzed35@gmail.com>
2022-08-17 10:02:48 -0400

    Finished the artist controller

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 #----------------------------------------------------------------------------#
 import collections
 import json
+import sys
 import dateutil.parser
 import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
@@ -405,7 +406,7 @@ def edit_venue_submission(venue_id):
     flash(f'An error occurred. Venue could not be changed.')
   if not error: 
     flash(f'Venue was successfully updated!')
-    
+
   return redirect(url_for('show_venue', venue_id=venue_id))
 
 #  Create Artist
@@ -418,14 +419,39 @@ def create_artist_form():
 
 @app.route('/artists/create', methods=['POST'])
 def create_artist_submission():
-  # called upon submitting the new artist listing form
-  # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
-
-  # on successful db insert, flash success
-  flash('Artist ' + request.form['name'] + ' was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
+  form = ArtistForm()
+  name = request.form.get('name')
+  city = request.form.get('city')
+  state = request.form.get('state')
+  phone = request.form.get('phone')
+  genres= request.form.getlist('genres')
+  image_link = request.form.get('image_link')
+  facebook_link = request.form.get('facebook_link')
+  website = request.form.get('website')
+  seeking_venue = True if 'seeking_venue' in request.form else False
+  seeking_description = request.form.get('seeking_description')
+  try:
+    artist = Artist(
+      name=name, 
+      city=city, 
+      state=state, 
+      phone=phone, 
+      genres=genres, 
+      facebook_link=facebook_link, 
+      image_link=image_link, 
+      website=website, 
+      seeking_venue=seeking_venue, 
+      seeking_description=seeking_description
+    )
+    db.session.add(artist)
+    db.session.commit()
+    flash('Artist ' + request.form['name'] + ' was successfully listed!')
+  except: 
+    flash('An error occurred. Artist ' + request.form['name']+ ' could not be listed.')
+    db.session.rollback()
+    print(sys.exc_info())
+  finally: 
+    db.session.close()
   return render_template('pages/home.html')
 
 
