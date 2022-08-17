867bdbb4558d6f7052c11cf075114c50826d6897
Joze <josephzed35@gmail.com>
2022-08-17 09:45:23 -0400

    Finished creating all the models

--- app.py
+++ app.py
@@ -41,11 +41,11 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-    # genres = db.Column(db.ARRAY(db.String))
-    # website = db.Column(db.String(120))
-    # seeking_talent = db.Column(db.Boolean, default=False)
-    # seeking_description = db.Column(db.String)
-    # shows = db.relationship('Show', backref='venue', lazy=True)
+    genres = db.Column(db.ARRAY(db.String))
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String)
+    shows = db.relationship('Show', backref='venue', lazy=True)
 
     def __repr__(self):
       return f'<Artist {self.id} {self.name}>'
@@ -60,25 +60,25 @@ class Artist(db.Model):
     phone = db.Column(db.String(120))
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
-    # facebook_link = db.Column(db.String(120))
-    # website = db.Column(db.String(120))
-    # seeking_venue = db.Column(db.Boolean, default=False)
-    # seeking_description = db.Column(db.String)
-    # shows = db.relationship('Show', backref='artist', lazy=True)
+    facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String)
+    shows = db.relationship('Show', backref='artist', lazy=True)
 
     def __repr__(self):
       return f'<Artist {self.id} {self.name}>'
 
-# class Show(db.Model):
-#   __tablename__ = 'shows'
+class Show(db.Model):
+  __tablename__ = 'shows'
 
-#   id = db.Column(db.Integer, primary_key=True)
-#   artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
-#   venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)
-#   start_time = db.Column(db.DateTime, nullable=False)
+  id = db.Column(db.Integer, primary_key=True)
+  artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
+  venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), nullable=False)
+  start_time = db.Column(db.DateTime, nullable=False)
 
-#   def __repr__(self):
-#     return f'<Show {self.id} {self.artist_id} {self.venue_id} {self.start_time}>'
+  def __repr__(self):
+    return f'<Show {self.id} {self.artist_id} {self.venue_id} {self.start_time}>'
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
