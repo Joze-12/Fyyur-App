38194af36a92db06caa9959c512c2c20fa292540
Joze <josephzed35@gmail.com>
2022-08-17 09:43:04 -0400

    Created the first Migrate

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ collections.Callable = collections.abc.Callable
 #----------------------------------------------------------------------------#
 
 class Venue(db.Model):
-    __tablename__ = 'Venue'
+    __tablename__ = 'venues'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -41,17 +41,17 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-    genres = db.Column(db.ARRAY(db.String))
-    website = db.Column(db.String(120))
-    seeking_talent = db.Column(db.Boolean, default=False)
-    seeking_description = db.Column(db.String)
-    shows = db.relationship('Show', backref='venue', lazy=True)
+    # genres = db.Column(db.ARRAY(db.String))
+    # website = db.Column(db.String(120))
+    # seeking_talent = db.Column(db.Boolean, default=False)
+    # seeking_description = db.Column(db.String)
+    # shows = db.relationship('Show', backref='venue', lazy=True)
 
     def __repr__(self):
       return f'<Artist {self.id} {self.name}>'
 
 class Artist(db.Model):
-    __tablename__ = 'Artist'
+    __tablename__ = 'artists'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -60,25 +60,25 @@ class Artist(db.Model):
     phone = db.Column(db.String(120))
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean, default=False)
-    seeking_description = db.Column(db.String)
-    shows = db.relationship('Show', backref='artist', lazy=True)
+    # facebook_link = db.Column(db.String(120))
+    # website = db.Column(db.String(120))
+    # seeking_venue = db.Column(db.Boolean, default=False)
+    # seeking_description = db.Column(db.String)
+    # shows = db.relationship('Show', backref='artist', lazy=True)
 
     def __repr__(self):
       return f'<Artist {self.id} {self.name}>'
-      
-class Show(db.Model):
-  __tablename__ = 'shows'
 
-  id = db.Column(db.Integer, primary_key=True)
-  artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
-  venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)
-  start_time = db.Column(db.DateTime, nullable=False)
+# class Show(db.Model):
+#   __tablename__ = 'shows'
 
-  def __repr__(self):
-    return f'<Show {self.id} {self.artist_id} {self.venue_id} {self.start_time}>'
+#   id = db.Column(db.Integer, primary_key=True)
+#   artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
+#   venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)
+#   start_time = db.Column(db.DateTime, nullable=False)
+
+#   def __repr__(self):
+#     return f'<Show {self.id} {self.artist_id} {self.venue_id} {self.start_time}>'
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
